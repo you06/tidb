3ebb5fb4d906cd8ffd3e866bec61c8af170c895b
you06 <you1474600@gmail.com>
2021-10-18 13:04:26 +0800

    enabel streaming
    Signed-off-by: you06 <you1474600@gmail.com>

--- executor/builder.go
+++ executor/builder.go
@@ -3243,6 +3243,9 @@ func buildNoRangeIndexLookUpReader(b *executorBuilder, v *plannercore.PhysicalIn
 	if err != nil {
 		return nil, err
 	}
+	if b.ctx.GetSessionVars().ConnectionID > 0 {
+		indexStreaming = true
+	}
 	tableReq, tableStreaming, tbl, err := buildTableReq(b, v.Schema().Len(), v.TablePlans)
 	if err != nil {
 		return nil, err
